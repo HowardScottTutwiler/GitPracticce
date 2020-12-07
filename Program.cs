79fb367a94d2f25dbd983e896eec47629a268ab5
Scott Tutwiler <hstutwiler@gmail.com>
2020-12-07 17:44:06 -0600

    git practice initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GitPracticce
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var auto = new List<int>() { 1, 2, 3 };
+            foreach (var item in auto)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
